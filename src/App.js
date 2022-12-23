33056c501a7b3cb6216d1fab55e28284c840a721
Prasham1710 <jainprasham17@gmail.com>
2022-12-23 22:35:39 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Login from './components/loginpage'
+import Signup from './components/signup'
 const App = () => {
   return (
-    <div><Login/></div>
+    <div><Login/>
+    <Signup/></div>
+    
   )
 }
 
